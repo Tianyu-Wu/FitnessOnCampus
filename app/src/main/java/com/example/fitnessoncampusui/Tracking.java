706e260b0f403cc45a0d1915e376c4b3fe46082d
Tianyu Wu <t228wu@uwaterloo.ca>
2019-03-31 17:12:46 +0200

    add load poi function to tracking

--- app/src/main/java/com/example/fitnessoncampusui/Tracking.java
+++ app/src/main/java/com/example/fitnessoncampusui/Tracking.java
@@ -8,10 +8,21 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class Tracking extends AppCompatActivity {
 
     private static final String TAG = "Tracking";
-    
+
+    // declare variables
+    private List<POI> POIs;
+
+
     LinearLayout back;
 
     @Override
@@ -30,5 +41,40 @@ public class Tracking extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        
+    }
+
+
+    private boolean loadPOI(List<POI> pois) {
+        try{
+            String line = "";
+            InputStream inputStream = getResources().openRawResource(R.raw.pois);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            int i = 0;
+            while((line = reader.readLine()) != null) {
+                if (i == 0) {
+                    ++i;
+                    continue;
+                }
+                String[] parse = line.split(";");
+                Log.d("MainActivity", parse[0]);
+                Log.d("MainActivity", parse[1]);
+                Log.d("MainActivity", parse[2]);
+                Log.d("MainActivity", parse[3]);
+
+                POI poi = new POI(parse[0], parse[1], Double.valueOf(parse[3]), Double.valueOf(parse[2]));
+                Log.d(TAG, "loadPOI: finished");;
+                pois.add(poi);
+                ++i;
+            }
+            return true;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
